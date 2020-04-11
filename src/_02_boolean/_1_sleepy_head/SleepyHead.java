ef482910d991190cf4ea4d74e3b62a82d95fd7c1
root <root@8280f3f34d75>
2020-04-11 18:34:37 +0000

    yayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayayayayayayayayayayayayayayayayaayyaayayayayayayyaayyayaayyaayayayayayayayaayayayayayaayayayayayayayaya

--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -16,13 +16,13 @@ public class SleepyHead {
          * Ask the user for these values using a confirm dialog like the one below
          * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
          */
-    
+JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
 
-
-        /*
+       }
+/*
          * Print “sleep in"? if it is a vacation or a weekend. 
          * If it’s a weekday, print “get up lazybones!"? 
          * If it is a weekday, but we are on vacation,  print “sleep in"?.
          */
     }
-}
+
