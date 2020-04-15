1675581fd6379ff268da1c01681decaec29b6864
root <root@e179dd190a74>
2020-04-15 23:54:45 +0000

    yayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayayaya

--- src/_02_boolean/_1_sleepy_head/SleepyHead.java
+++ src/_02_boolean/_1_sleepy_head/SleepyHead.java
@@ -2,27 +2,45 @@ package _02_boolean._1_sleepy_head;
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		boolean isWeekday, isVacation;
 
-       }
-/*
-         * Print “sleep in"? if it is a vacation or a weekend. 
-         * If it’s a weekday, print “get up lazybones!"? 
-         * If it is a weekday, but we are on vacation,  print “sleep in"?.
-         */
-    }
+		/* You MUST use the above boolean variables in your code */
 
+		/*
+		 * Ask the user for these values using a confirm dialog like the one below
+		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
+		 * JOptionPane.YES_NO_OPTION);
+		 */
+		int answer = JOptionPane.showConfirmDialog(null, "Is it a weekday?");
+//yes =0 no =1
+		if (answer == 0) {
+			isWeekday = true;
+
+		} else {
+			isWeekday = false;
+		}
+		int answerNumberOne = JOptionPane.showConfirmDialog(null, "Is it a vacation?");
+		if (answerNumberOne == 0) {
+			isVacation = true;
+		} else {
+			isVacation = false;
+		}
+		if (isWeekday = false || isVacation == true) {
+			JOptionPane.showMessageDialog(null, "SLEEP IN, THEN!!");
+		} else if (isWeekday == true) {
+			JOptionPane.showMessageDialog(null, "GET UP!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		} else if (isVacation == true && isWeekday == true) {
+
+		}
+
+	}
+	/*
+	 * Print “sleep in"? if it is a vacation or a weekend. If it’s a weekday, print
+	 * “get up lazybones!"? If it is a weekday, but we are on vacation, print “sleep
+	 * in"?.
+	 */
+}
--- src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
+++ src/_02_boolean/_2_googly_eyes/googly_eyes/googly_eyes.pde
@@ -1,8 +1,24 @@
+PImage face;
 
 void setup() {
-  
+  //print("HELLLOLLOLOLOLOLOLOLOLOLOLOLOLOL"); 
+
+size(840, 770);
+face = loadImage("tomnaughtyface.png");
+ face.resize(840,770);
+
 }
 
 void draw() {
-  
+  background(face);
+fill(#ffffff);
+ellipse();
+ellipse(200,450,60,90);
+fill(#000000);
+ellipse(200,450,30,45);
+
+
+
+
+
 }
